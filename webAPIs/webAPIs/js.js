82ae965d5bf7c946f24e8764cedcd9194cb95998
2152762 <35756087+2152762@users.noreply.github.com>
2019-03-31 00:26:45 +0800

    web api search function

---
+++ webAPIs/webAPIs/js.js
@@ -0,0 +1,42 @@
+        let baseURL = 'https://api.themoviedb.org/3/';
+        let configData = null;
+        let baseImageURL = "http://image.tmdb.org/t/p/";
+        let APIKEY = '32bb73effdd6a18cf39e0cfbe164c2c1';
+//        let image = "http://image.tmdb.org/t/p/";
+        let getConfig = function () {
+            
+            let url = "".concat(baseURL, 'configuration?api_key=', APIKEY); 
+            fetch(url)
+            .then((result)=>{
+                return result.json();
+            })
+            .then((data)=>{
+            var keyword = document.getElementById("keyword").value;
+                baseImageURL = data.images.secure_base_url;
+                configData = data.images;
+                console.log('config:', data);
+                console.log('config fetched');
+                console.log('keyword:', keyword)
+                console.log(data.poster_path)
+                runSearch(keyword)
+            })
+            .catch(function(err){
+                alert(err);
+            });
+        }
+    
+        
+        
+        let runSearch = function (keyword) {
+            let url = ''.concat(baseURL, 'search/movie?api_key=', APIKEY, '&query=', keyword);
+            fetch(url)
+            .then(result=>result.json())
+            .then((data)=>{
+                //process the returned data  
+                document.getElementById('output').innerHTML = JSON.stringify(data, null, 4);
+                document.getElementsById('img').src = data.poster_path;
+                //work with results array...
+            })
+//            document.addEventListener('DOMContentLoaded');
+        }
+//        getConfig();
