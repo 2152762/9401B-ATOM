8ba55db36ceb37401bb03a44c0cc02034827a30d
2152762 <35756087+2152762@users.noreply.github.com>
2019-04-07 02:04:16 +0800

    Add files via upload

--- Bootstrap html/js.js
+++ Bootstrap html/js.js
@@ -2,32 +2,7 @@
         let configData = null;
         let baseImageURL = "http://image.tmdb.org/t/p/";
         let APIKEY = '32bb73effdd6a18cf39e0cfbe164c2c1';
-//        let image = "http://image.tmdb.org/t/p/";
-      
-//        let getConfig = function () {
-//            
-//            let url = "".concat(baseURL, 'configuration?api_key=', APIKEY); 
-//            fetch(url)
-//            .then((result)=>{
-//                return result.json();
-//            })
-//            .then((data)=>{
-//            var keyword = document.getElementById("keyword").value;
-//                baseImageURL = data.images.secure_base_url;
-//                configData = data.images;
-////                console.log('config:', data);
-////                console.log('config fetched');
-////                console.log('keyword:', keyword)
-////                console.log(data.poster_path)
-//                runSearch(keyword)
-//            })
-//            .catch(function(err){
-//                alert(err);
-//            });
-//        }
-    
-        
-        
+        let dataPage = 1;
         let popularMovies = function () {
             let url = ''.concat(baseURL, 'movie/popular?api_key=', APIKEY,'&page=1');
             fetch(url)
@@ -38,18 +13,12 @@
 //                let over = document.getElementsByClassName('over')
                 var res = data.results;
             for (var i = 0; i < res.length; i++) {
-                // get the size of the inner array
-               console.log(res[i])
              
             }
              
-//            for (var x = 0; x < over.length; x++){
-//                over[x].innerHTML = res[x].overview;
-//                
-//            }
             for(var z = 0; z < imgs.length; z++){
                 imgs[z].src = ''.concat(baseImageURL,'original',res[z].poster_path);
-                console.log(imgs[z])
+//                console.log(imgs[z])
             }
                             
                 //work with results array...
@@ -70,13 +39,13 @@
                 var res = data.results;
             for (var i = 0; i < res.length; i++) {
                 // get the size of the inner array
-               console.log(res[i])
+//               console.log(res[i])
              
             }
              
             for(var z = 0; z < imgs.length; z++){
                 imgs[z].src = ''.concat(baseImageURL,'original',res[z].backdrop_path);
-                console.log(imgs[z])
+//                console.log(imgs[z])
             }
 
                 //work with results array...
@@ -87,7 +56,13 @@
         upcoming();
 
     let topRated = function () {
-            let url = ''.concat(baseURL, 'movie/top_rated?api_key=', APIKEY,'&page=1');
+
+        nextPage = function (){
+            if(dataPage >= 1){
+                     dataPage += 1
+                     console.log('next',dataPage);
+            
+            let url = ''.concat(baseURL, 'movie/top_rated?api_key=', APIKEY,'&page=', dataPage);
             fetch(url)
             .then(result=>result.json())
             .then((data)=>{
@@ -96,18 +71,48 @@
                 var res = data.results;
             for (var i = 0; i < res.length; i++) {
                 // get the size of the inner array
-               console.log(res[i])
+//               console.log(res[i])
              
             }
              
             for(var z = 0; z < imgs.length; z++){
                 imgs[z].src = ''.concat(baseImageURL,'original',res[z].poster_path);
-                console.log(imgs[z])
+//                console.log(imgs[z])
             }
 
                 //work with results array...
             })
 //            document.addEventListener('DOMContentLoaded');
             
-        } 
+            }
+        }
+        previousPage = function() {
+            
+                if(dataPage >= 1){
+                    dataPage -= 1
+                    console.log('prev',dataPage)
+        
+        let url = ''.concat(baseURL, 'movie/top_rated?api_key=', APIKEY,'&page=', dataPage);
+            fetch(url)
+            .then(result=>result.json())
+            .then((data)=>{
+                let imgs  = document.getElementsByClassName('top');
+                
+                var res = data.results;
+                for (var i = 0; i < res.length; i++) {
+                // get the size of the inner array
+               console.log(res[i])
+                }
+             
+            for(var z = 0; z < imgs.length; z++){
+                imgs[z].src = ''.concat(baseImageURL,'original',res[z].poster_path);
+//                console.log(imgs[z])
+                }
+            })
+//            document.addEventListener('DOMContentLoaded');
+        }
+        }
+    }
         topRated();
+
+    
